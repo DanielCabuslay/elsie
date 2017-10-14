3e84fbcd4d283ae2646d340ba49d5640b23a9250
Daniel Cabuslay <ddccabuslay@gmail.com>
2017-10-14 11:13:16 -0400

    Cleaned up unnecessary code

--- scripts/toolbar.js
+++ scripts/toolbar.js
@@ -9,13 +9,6 @@
     }, 250);
     function init() {
         var toolbar = mdc.toolbar.MDCToolbar.attachTo(document.querySelector('.mdc-toolbar'));
-        toolbar.listen('MDCToolbar:change', function(evt) {
-            var flexibleExpansionRatio = evt.detail.flexibleExpansionRatio;
-        });
         toolbar.fixedAdjustElement = document.querySelector('.mdc-toolbar-fixed-adjust');
-        // toolbar.fixedAdjustElement = document.querySelector('#search_body');
-        // toolbarHeight = $('.mdc-toolbar-fixed-adjust').css('margin-top');
-        // $('.mdc-toolbar-fixed-adjust').css('height', 'calc(100vh - ' + toolbarHeight + ')');
-        // $('#search_body').css('height', 'calc(100vh - ' + toolbarHeight + ')');
     }
 })();
