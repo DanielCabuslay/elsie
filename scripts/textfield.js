4d7a1257b910f3fb63884a6433fd66475fb20df6
Daniel Cabuslay <ddccabuslay@gmail.com>
2017-12-30 14:13:05 -0500

    Disabled inputs on username submit

--- scripts/textfield.js
+++ scripts/textfield.js
@@ -3,4 +3,9 @@
     for (var i = 0, tf; tf = tfs[i]; i++) {
       mdc.textField.MDCTextField.attachTo(tf);
     }
-  })();
+})();
+
+$('#mal_login').submit(function() {
+	var inputs = $(this).find("input");
+	inputs.prop("disabled", true);
+});
