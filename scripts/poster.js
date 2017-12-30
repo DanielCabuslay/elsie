a145fb14b0e285b12216c7870fb044b2b55d016c
Daniel Cabuslay <ddccabuslay@gmail.com>
2017-12-30 14:54:02 -0500

    Clicking outside of poster will close poster view

--- scripts/poster.js
+++ scripts/poster.js
@@ -2,6 +2,6 @@ $('#mal_poster').click(function() {
 	$('#poster_background').fadeIn();
 });
 
-$('#poster_background i').click(function() {
+$('#poster_background i, #poster_background').click(function() {
 	$('#poster_background').fadeOut();
 });
