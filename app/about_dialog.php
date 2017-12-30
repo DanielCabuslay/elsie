a84732691cdeb52037db47e26976b76bb856ee1c
Daniel Cabuslay <ddccabuslay@gmail.com>
2017-12-30 15:27:34 -0500

    Updated about blurb to highlight external links addition from AniList

--- app/about_dialog.php
+++ app/about_dialog.php
@@ -4,7 +4,7 @@
         <div><h2 class="mdc-dialog__header__title">About Elsie</h2></div>
     </header>
     <section class="mdc-dialog__body">
-      Elsie is a Material Design MyAnimeList User List Viewer. Users can browse their anime list and access anime pages on MyAnimeList. Additional data is also pulled from AniList when available, such as upcoming local episode times (based on airing times in Japan) and Twitter hashtags for shows.
+      Elsie is a Material Design MyAnimeList User List Viewer. Users can browse their anime list and access anime pages on MyAnimeList. Additional data is also pulled from AniList when available, such as upcoming local episode times (based on airing times in Japan) and streaming links for shows.
     </section>
     <footer class="mdc-dialog__footer">
       <a type="button" class="mdc-button mdc-dialog__footer__button" target="_blank" href="https://ddccabuslay.me">My Website</a>
